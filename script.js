6cd16742c6525f54dc2e38debe2c12e0ba2258e6
Corey Donahue <137344214+CDarts48@users.noreply.github.com>
2023-11-15 14:12:42 -0700

    working on the saveBtn

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+console.log("hello world")
+var saveBtn = $(".saveBtn");
+var textArea = $(".textarea");
+$(document).ready()
+
+function save () {
+  saveBtn.click(function() {
+    {
+      $.localStorage.set("textArea", textArea.val());
+      console.log(localStorage)
+    }
+
+  });
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +33,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+};
